333914a62b44df1e59b3f70a6a119abe7e61d349
Begal Wadim <begal-vadim98@yandex.ru>
2022-02-10 13:45:39 +0300

    Добавил подпись %

--- script.js
+++ script.js
@@ -15,21 +15,23 @@ const myEventsFunction = function () {
     }
 
   }
-  
+
   btn.addEventListener('click', changeColor);
 
   eBtn.style.display = "none";
 
   // input range
-  rangeSpan.textContent = inputTypeRange.value;
+  rangeSpan.textContent = inputTypeRange.value + "%";
   circle.style.width = inputTypeRange.value + "%";
   circle.style.height = inputTypeRange.value + "%";
 
-  const changeRange = function () {
-    rangeSpan.textContent = inputTypeRange.value;
+  const changeRange = function (e) {
+
+    rangeSpan.textContent = inputTypeRange.value + "%";
     circle.style.width = inputTypeRange.value + "%";
     circle.style.height = inputTypeRange.value + "%";
     circle.style.backgroundColor = `rgba(255,255,${inputTypeRange.value})`;
+
   }
   inputTypeRange.addEventListener('input', changeRange);
 }
